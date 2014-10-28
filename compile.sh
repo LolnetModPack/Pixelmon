f219a1db7019052cff7b7dcb65853faff3a9965f
James137137 <james137137@gmail.com>
2014-10-29 08:35:17 +1300

    First upload no src folder yet

---
+++ compile.sh
@@ -0,0 +1,14 @@
+array=( $(< BuildNumber) )
+sum=$( IFS=+; echo "${array[*]}" | bc )
+echo "$(($sum+1))" > BuildNumber
+
+java -cp LolnetLauncher.jar com.skcraft.launcher.builder.PackageBuilder --version "1.2.2" --manifest-dest "dist/manifest.json" --objects-dest "dist/objects" --files "src" --config "modpack.json" --objects-url "https://lolnet.co.nz/modpack/objects" --version-file "version.json" --libs-url "https://lolnet.co.nz/modpack/libraries"
+
+
+cp -r /var/www/modpack/Setup/Pixelmon/dist/objects/ /var/www/modpack/
+cp -r /var/www/modpack/Setup/Pixelmon/dist/manifest.json /var/www/modpack/Pixelmonmanifest.json
+rm -r /var/www/modpack/Setup/Pixelmon/dist/
+
+
+cp CompletedTemplate.json ../../public/Pixelmon.json
+rm CompletedTemplate.json
